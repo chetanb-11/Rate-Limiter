02f01bfe5d2ac56a4e3465bb5ad671baaa302ae4
Chetan <chetan87706@gmail.com>
2026-03-30 04:55:46 +0530

    added proxy middleware

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 const express = require('express');
 const ip = require('ip');
 const redis = require('./redis');
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const MAX_ALLOWED_REQ = 5;
-const MAX_TIME = 5;
+const MAX_TIME = 20;
 
-// let ip_mapping = {}
 
 const app = express();
 
-// setInterval(() => {
-//     ip_mapping = {};
-//     console.log("ip mapping clear");
-// }, MAX_TIME)
-
-app.use(async(req, res, next)=>{
+const rateLimiter = async(req, res, next) => {
     const my_ip = ip.address();
 
     // increment out ip request
-    const request = await redis.incr(my_ip);
-    // ip_mapping[my_ip] = ip_mapping[my_ip] + 1 || 1;
+    const request = await redis.incr(my_ip); // ip_mapping[my_ip] = ip_mapping[my_ip] + 1 || 1;
 
-    if(request === 1){
+    if (request === 1) {
+        console.log("clearing mapping");
         await redis.expire(my_ip, MAX_TIME);
     }
 
-    // console.log(`recieved request no ${ip_mapping[my_ip]} from ${my_ip}`);
+    console.log(`recieved request no ${request} from ${my_ip}`);
 
-    if(request > MAX_ALLOWED_REQ){
-        return res.status(429).json({message:"too many request"}); 
+    if (request > MAX_ALLOWED_REQ) {
+        return res.status(429).json({
+            error: "too many request",
+            message: `You have exceeded the ${MAX_ALLOWED_REQ} requests in ${MAX_TIME} seconds limit.`
+        });
     }
 
     // if(ip_mapping[my_ip] > MAX_ALLOWED_REQ){
@@ -36,15 +33,19 @@ app.use(async(req, res, next)=>{
     //     res.status(429).send("Too many requests");
     // }
     next();
-})
+}
 
-app.get("/", (req, res) => {
-    // console.log("received a request");
+app.use(rateLimiter);
 
-    // const my_ip = ip.address();
-    // console.log(my_ip);
-
-    res.status(200).send("ok");
-})
+app.use("/api", createProxyMiddleware({
+    target: 'https://jsonplaceholder.typicode.com',
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api' : '',
+    },
+    onProxyResponse: function(proxyRes, req, res) {
+        console.log(`Successfully proxied request from ${ip.address()}`);
+    }
+}))
 
 app.listen(3000, () => console.log("app running on localhost:3000"));
